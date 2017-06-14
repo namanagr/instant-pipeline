1709cbe9b1d87aee314c4bc7f1a7d8778cdc1de4
vagrant <namanagr@gmail.com>
2017-06-14 14:17:26 -0700

    adding working gogs directory that has sample flash application

--- vagrant-script.sh
+++ vagrant-script.sh
@@ -9,16 +9,8 @@ echo 'alias docker-compose="sudo docker-compose"' >> /home/vagrant/.bash_profile
 
 mkdir pipeline-project 2> /dev/null # Create the project folder that contains all the applications.
 cd pipeline-project # Enter the project folder
-wget https://dl.gogs.io/0.11.19/linux_amd64.tar.gz # Get the latest release of gogs
-tar -xzf linux_amd64.tar.gz # Unarchive 
-rm linux_amd64.tar.gz # Delete the archived gogs folder 
 
-#cp /home/vagrant/instant-pipeline/gogs.db /home/vagrant/pipeline-project/gogs/
-#cp /home/vagrant/instant-pipeline/app.ini /home/vagrant/pipeline-project/gogs/custom/conf/
-
-sudo mkdir -p /var/log/gogs
-
-mkdir -p /home/vagrant/pipeline-project/gogs/custom/conf
-cp /vagrant/app.ini /home/vagrant/pipeline-project/gogs/custom/conf/
-/home/vagrant/pipeline-project/gogs/gogs web & # Start the gogs server. It will listen on 0.0.0.0:3000
+cp -R /vagrant/instant-pipeline/gogs /home/vagrant/pipeline-project/
+cd gogs
+./gogs web & # Start the gogs server. It will listen on 0.0.0.0:3000
 
