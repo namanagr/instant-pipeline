0bc6a075eb118e6245c0286330b3436e4e96b187
vagrant <namanagr@gmail.com>
2017-06-16 16:18:18 -0700

    working setup 1

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,20 +6,20 @@ apt-get -y update
 #apt-get -y upgrade
 apt-get -y install jenkins
 #apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-#apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#apt-get -y update
-#apt-get -y install docker-ce
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get -y update
+apt-get -y install docker-ce
 #apt-get -y install docker-compose
 
 # Install and setup sqlite database
 sudo apt-get -y install sqlite3 libsqlite3-dev
 
-#cp -R /vagrant/instant-pipeline/jenkins /var/lib/
-#chown -R jenkins /var/lib/jenkins
-#chgrp -R jenkins /var/lib/jenkins
+cp -R /vagrant/instant-pipeline/jenkins /var/lib/
+chown -R jenkins /var/lib/jenkins
+chgrp -R jenkins /var/lib/jenkins
 service jenkins restart
 
-sudo sh -c 'echo \"jenkins ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers'
+sudo sh -c 'echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
 sudo apt-get -y install supervisor
